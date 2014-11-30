b01ce3fb1d907b95f91a19fe81a2334574e75df4
Mangesh Yadav <mangesh.yadav@hotmail.com>
2014-11-29 17:26:45 -0800

    Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ip_address', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.post('/GumballAction',routes.GumballAction);
+
+http.createServer(app).listen(app.get('port'), app.get('ip_address'),function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,111 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res) {
+	var http = require('http');
+	var Client = require('node-rest-client').Client;
+	var client = new Client();
+
+	// direct way
+	client.get("http://mangesh-grails-gumball-v2.cfapps.io/gumballs.json", function(data,
+			response) {
+
+		var arrayData = {};
+
+		arrayData = data;
+		
+
+		var messages = [];
+
+		messages.push(arrayData[0].modelNumber);
+		messages.push(arrayData[0].serialNumber);
+		messages.push("NoCoinState");
+		messages.push(arrayData[0].countGumballs);
+
+		res.render('index', {
+			message : messages
+		});
+
+	});
+
+};
+
+exports.GumballAction = function(req, res) {
+
+	var event = req.param('event');
+	var state = req.param('state');
+	var count = req.param('count');
+	var modelNumber = req.param('modelNumber');
+	var serialNumber = req.param('serialNumber');
+
+	// var message=req.param('message');
+	if (event === 'InsertQuater' && state === 'NoCoinState') {
+
+		state = 'HasACoinState';
+		var messagesToBePut = [];
+
+		messagesToBePut.push(modelNumber);
+		messagesToBePut.push(serialNumber);
+		messagesToBePut.push(state);
+		messagesToBePut.push(count);
+		res.render('index', {
+			message : messagesToBePut
+		});
+
+	}
+	if (event === 'TurnTheCrank' && state === 'HasACoinState') {
+		var messagesToBePutInPost = [];
+		var Client = require('node-rest-client').Client;
+		var client = new Client();
+		client.get("http://mangesh-grails-gumball-v2.cfapps.io/gumballs.json", function(data,
+				response) {
+			var ar = {};
+
+			ar = data;
+
+			var count = ar[0].countGumballs;
+
+			if (count !== 0) {
+				count = count - 1;
+				var args = {
+					data : {
+						countGumballs : count
+					},
+					headers : {
+						"Content-Type" : "application/json"
+					}
+				};
+				client.put("http://mangesh-grails-gumball-v2.cfapps.io/gumballs/1", args,
+						function(data, response) {
+							// parsed response body as js object
+							console.log(data);
+							// raw response
+							console.log(response);
+							var messageToBePut = [];
+							messagesToBePutInPost.push(modelNumber);
+							messagesToBePutInPost.push(serialNumber);
+							messagesToBePutInPost.push("NoCoinState");
+							messagesToBePutInPost.push(count);
+							res.render('index', {
+								message : messagesToBePutInPost
+							});
+						});
+
+			}else{
+				
+				var messageToBePut = [];
+				messagesToBePutInPost.push(modelNumber);
+				messagesToBePutInPost.push(serialNumber);
+				messagesToBePutInPost.push("OutOfStock");
+				messagesToBePutInPost.push(0);
+				res.render('index', {
+					message : messagesToBePutInPost
+				});
+			}
+
+		});
+
+	}
+
+};
